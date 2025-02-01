76f3ec2da7b2429d31e85e91131e16b7309e27ae
Pabitra Sahoo <sahoopabitra596@gmail.com>
2025-02-01 23:43:18 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
-import Editor from '@monaco-editor/react'
+import Editor from "@monaco-editor/react";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://codeconnect-by-team-seven.onrender.com");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
@@ -15,8 +15,7 @@ const App = () => {
   const [users, setUsers] = useState([]);
   const [typing, setTyping] = useState("");
   const [outPut, setOutPut] = useState("");
-  const [version,setVersion] = useState("*");
-
+  const [version, setVersion] = useState("*");
 
   useEffect(() => {
     socket.on("userJoined", (users) => {
@@ -37,7 +36,7 @@ const App = () => {
     });
 
     socket.on("codeResponse", (respose) => {
-      setOutPut(respose.run.output)
+      setOutPut(respose.run.output);
     });
 
     return () => {
@@ -52,11 +51,11 @@ const App = () => {
   useEffect(() => {
     const handleBeforeUnload = () => {
       socket.emit("leaveRoom");
-    }
+    };
 
-    window.addEventListener("beforeunload", handleBeforeUnload)
+    window.addEventListener("beforeunload", handleBeforeUnload);
     return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload)
+      window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
 
@@ -70,16 +69,16 @@ const App = () => {
   const leaveRoom = () => {
     socket.emit("leaveRoom");
     setJoined(false);
-    setRoomId("")
-    setUserName("")
-    setCode("")
-    setLanguage("javascript")
-  }
+    setRoomId("");
+    setUserName("");
+    setCode("");
+    setLanguage("javascript");
+  };
 
   const copyRoomId = () => {
-    navigator.clipboard.writeText(roomId)
-    setCopySucccess("Copied!")
-    setTimeout(() => setCopySucccess(""), 2000)
+    navigator.clipboard.writeText(roomId);
+    setCopySucccess("Copied!");
+    setTimeout(() => setCopySucccess(""), 2000);
   };
 
   const handlecodechange = (newCode) => {
@@ -88,15 +87,15 @@ const App = () => {
     socket.emit("typing", { roomId, userName });
   };
 
-  const handleLanguageChange = e => {
-    const newLanguage = e.target.value
+  const handleLanguageChange = (e) => {
+    const newLanguage = e.target.value;
     setLanguage(newLanguage);
     socket.emit("languageChange", { roomId, language: newLanguage });
   };
 
   const runCode = () => {
-    socket.emit("compileCode", { code, roomId, language, version});
-  }
+    socket.emit("compileCode", { code, roomId, language, version });
+  };
 
   const generateUniqueId = () => {
     const uniqueId = Math.random().toString(36).substr(2, 9);
@@ -107,23 +106,40 @@ const App = () => {
     return (
       <div className="join">
         <header>
-          <h2 className="logo"><b className="c">C</b>ode<b className="c">C</b>onnect</h2>
+          <h2 className="logo">
+            <b className="c">C</b>ode<b className="c">C</b>onnect
+          </h2>
           <nav className="navigation">
-            <a href="#"><b>About</b></a>
-            <a href="#"><b>Developers</b></a>
+            <a href="#">
+              <b>About</b>
+            </a>
+            <a href="#">
+              <b>Developers</b>
+            </a>
           </nav>
         </header>
         <div className="join-cointainer">
           <div className="join-form">
             <h1>Code-Room</h1>
-            <input className="rid" type="text" placeholder="Room Id" value={roomId} onChange={e => setRoomId(e.target.value)} />
-            
-            <input className="nm" type="text" placeholder="Your Name" value={userName} onChange={e => setUserName(e.target.value)} />
+            <input
+              className="rid"
+              type="text"
+              placeholder="Room Id"
+              value={roomId}
+              onChange={(e) => setRoomId(e.target.value)}
+            />
+
+            <input
+              className="nm"
+              type="text"
+              placeholder="Your Name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
             <button onClick={joinRoom}>Join Room</button>
-            <a className="uid"onClick={generateUniqueId}>Generate unique room id.</a>
-           
-          
-       
+            <a className="uid" onClick={generateUniqueId}>
+              Generate unique room id.
+            </a>
           </div>
         </div>
         <footer>Developed & Maintained by Team-Seven © 2025</footer>
@@ -136,28 +152,35 @@ const App = () => {
         <div className="room-info">
           <h2>Room ID:</h2>
           <p className="rd">{roomId}</p>
-          <button onClick={copyRoomId} className="copy-button">Copy Room Id</button>
+          <button onClick={copyRoomId} className="copy-button">
+            Copy Room Id
+          </button>
           {copySuccess && <span className="copy-success">{copySuccess}</span>}
         </div>
         <h3>Users in Room:</h3>
         <ul>
-          {
-            users.map((user, index) => (
-              <li key={index}>{user.slice(0, 8)}</li>
-            ))
-          }
+          {users.map((user, index) => (
+            <li key={index}>{user.slice(0, 8)}</li>
+          ))}
         </ul>
         <h3>Select Language:</h3>
-        <select className="language-selector" value={language}
-          onChange={handleLanguageChange}>
+        <select
+          className="language-selector"
+          value={language}
+          onChange={handleLanguageChange}
+        >
           <option value="javascript">javascript</option>
           <option value="python">python</option>
           <option value="java">java</option>
           <option value="cpp">C++</option>
         </select>
-        <button className="run-btn" onClick={runCode}><b>Run</b></button>
+        <button className="run-btn" onClick={runCode}>
+          <b>Run</b>
+        </button>
         <p className="typing-indicator">{typing}</p>
-        <button className="leave-button" onClick={leaveRoom}>Leave Room</button>
+        <button className="leave-button" onClick={leaveRoom}>
+          Leave Room
+        </button>
       </div>
       <div className="editor-wrapper">
         <Editor
@@ -172,11 +195,16 @@ const App = () => {
             fontsize: 15,
           }}
         />
-        <textarea className="output-console" value={outPut} readOnly placeholder="Output will appear here..." />
+        <textarea
+          className="output-console"
+          value={outPut}
+          readOnly
+          placeholder="Output will appear here..."
+        />
       </div>
     </div>
   );
 };
 
 export default App;
-console.log('Socket connected to: ', socket);
+console.log("Socket connected to: ", socket);
